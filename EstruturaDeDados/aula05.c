4f1acfa31c14c34ec3d76a4e3565697334298749
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2025-03-06 21:19:11 -0300

    Atividades atualizada 06/02/2025
    Estrutura de dados

---
+++ EstruturaDeDados/aula05.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct pessoa {
+    char nome[50], cpf[15], rg[15], cel[16], fixo[16];
+    char endereco[50], bairro[30], cidade[30], uf[3];
+    int idade;
+};
+int main (){
+    struct pessoa cl[50];
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
