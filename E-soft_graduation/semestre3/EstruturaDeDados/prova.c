3fb4d0651e4a3f96358685c400ed8dac59cf29da
NicolasKauan <nicolaskauanpro@gmail.com>
2025-03-27 19:24:46 -0300

    First Pokedex and List
    Front-end 26/02/2025

---
+++ E-soft_graduation/semestre3/EstruturaDeDados/prova.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char n[10]="sophia";
+    int t = strlen(n);
+    for (int i = 0;i<t;i++){
+        printf("%c\n",n[i]);
+    }
+    
+
+    
+
+
+    return 0;
+}
